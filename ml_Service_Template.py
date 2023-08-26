93d48a93f8a57f14ad2309c276382339bb29daf8
“John <jhebeler@gmail.com>
2023-08-26 23:58:49 +0000

    better cohesion

--- ml_Service_Template.py
+++ ml_Service_Template.py
@@ -8,24 +8,12 @@ app = Flask(__name__)
 
 #http://localhost:8786/infer?age=28&salary=40000
 
-global rf
-global ml_learn
-ml_learn = False
-
 @app.route('/stats', methods=['GET'])
 def getStats():
-    global ml_learn
-    if(ml_learn == False):
-        #must learn first
-        ml_learn = rf.model_learn()
-    return rf.model_stats()
+    return str(rf.model_stats())
 
 @app.route('/infer', methods=['GET'])
 def getInfer():
-    global ml_learn
-    if(ml_learn == False):
-        #must learn first
-        ml_learn = rf.model_learn()
     args = request.args
     age = int(args.get('age'))
     salary = int(args.get('salary'))
@@ -40,8 +28,6 @@ def hellopost():
     print("Name: ", name, " Location: ", location)
     print("Image: ", image)
     return 'Hello, Post!'
-
-
 
 if __name__ == "__main__":
     flaskPort = 8786
--- randomforest.py
+++ randomforest.py
@@ -4,6 +4,9 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 
 
 class randomforest:
+    def __init__(self):
+        self.modelLearn = False
+        self.stats = 0
     def model_learn(self):
         print('Model Learning')
         # Importing the dataset
@@ -16,35 +19,34 @@ class randomforest:
         from sklearn.model_selection import train_test_split
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-        global sc
-
         # Feature Scaling
         from sklearn.preprocessing import StandardScaler
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-
-        global classifier
+        self.sc = StandardScaler()
+        X_train = self.sc.fit_transform(X_train)
+        X_test = self.sc.transform(X_test)
 
         # Training the Random Forest Classification model on the Training set
         from sklearn.ensemble import RandomForestClassifier
-        classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
-        classifier.fit(X_train, y_train)
+        self.classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+        self.classifier.fit(X_train, y_train)
 
         # Predicting the Test set results
-        y_pred = classifier.predict(X_test)
-
-        global stats 
-        stats = str(accuracy_score(y_test, y_pred))
-
+        y_pred = self.classifier.predict(X_test)
+        self.stats = str(accuracy_score(y_test, y_pred))
         return True
 
     def model_infer(self, age,salary):
-        if( classifier.predict(sc.transform([[age,salary]]))[0] == 1):
+        if(self.modelLearn == False):
+            #must learn first
+            self.modelLearn = self.model_learn()
+        if( self.classifier.predict(self.sc.transform([[age,salary]]))[0] == 1):
             determination = 'Yes, A good Candidate'
         else:
             determination = 'No, a Bad Candidate'
         return determination
 
     def model_stats(self):
-        return stats
+        if(self.modelLearn == False):
+            #must learn first
+            self.modelLearn = self.model_learn()
+        return self.stats
